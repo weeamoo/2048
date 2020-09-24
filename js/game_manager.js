4f106ac2778fd8bf9eb57c64044db3a02c8eddac
Weeamoo <github@surfinontheinter.net>
2020-09-24 12:56:05 -0500

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -154,7 +154,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 1.1);
+          var merged = new Tile(positions.next, tile.value * 0.9);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
